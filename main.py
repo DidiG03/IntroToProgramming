01a42f0ad47cda3aa56e744c6e026db65625e9dc
DidiG03 <sefridkapllani@gmail.com>
2024-04-04 11:38:05 +0100

    Creating the bacground
    Here I'm putting the background in the game. Setting up the enviroment. The asset folder will be included in the end

--- main.py
+++ main.py
@@ -8,17 +8,45 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-BG_COLOR = (255,255,255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
 
-window = pygame.display.set_mode(WIDTH, HEIGHT)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width +1):
+        for j in range (HEIGHT // height+1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    
+    return tiles, image
+
+def draw(window, bacground, bg_image):
+    for tile in bacground:
+        window.blit(bg_image, tile)
+    pygame.display.update()
 
 def main(window):
-    pass
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
+    run = True
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            
+        draw(window, background, bg_image)
+    pygame.quit()
+    quit()
 
 if __name__ == "__main__":
     main(window)
 
-    
